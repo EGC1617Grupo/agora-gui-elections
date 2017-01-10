7129c0a039b01a036447733aa81bb04192e16038
Daniel Moreno <danimoreno.m08@gmail.com>
2017-01-10 09:29:48 +0100

    refactor: I've changed the test because it did not work. Now, the test works only when the election with id 15 is created and the results of the election have been published

--- test/dynamic-directive-webspec.js
+++ test/dynamic-directive-webspec.js
@@ -1,9 +1,9 @@
 /* jshint ignore:start */
 describe("dynamic-directive tests", function () {
- 
 
 
-  it("should login as admin", function(){
+
+  it("should login as admin", function () {
     browser.get('/admin/login')
     var email = element(by.id('emailText'));
     email.sendKeys('admin@agoravoting.com');
@@ -16,35 +16,29 @@ describe("dynamic-directive tests", function () {
     expect(element(by.css('.input-group .form-control:first-child')).isDisplayed()).toBe(true);
   })
 
-  it("should switch between tabs and check the content is correct", function(){
+  //This test works only when the election with id 15 is created and the results of the election have been published
+  it("should switch between tabs and check the content is correct", function () {
     browser.get('/election/15/public/home')
-    
-    let status = element.all(by.tagName('strong'));
-    var strong = status.get(1).getText();
-    if(strong!="results published"){
-      expect(true).toBe(true);
-    }else{
-      var table = element(by.css('.table'));
-      expect(table.isPresent()).toBe(true);
-      var chart = element(by.css('.nvd3-svg'));
-      expect(chart.isPresent()).toBe(false);
 
-      let list = element.all(by.css('.nav-tabs li'));
+    var table = element(by.css('.table'));
+    expect(table.isPresent()).toBe(true);
+    var chart = element(by.css('.nvd3-svg'));
+    expect(chart.isPresent()).toBe(false);
 
-      list.get(1).click();
-      var table = element(by.css('.table'));
-      expect(table.isPresent()).toBe(false);
-      var chart = element(by.css('.nvd3-svg'));
-      expect(chart.isPresent()).toBe(true);
-      
-      list.get(0).click();
-      var table = element(by.css('.table'));
-      expect(table.isPresent()).toBe(true);
-      var chart = element(by.css('.nvd3-svg'));
-      expect(chart.isPresent()).toBe(false);
-    }
+    let list = element.all(by.css('.nav-tabs li'));
+
+    list.get(1).click();
+    var table = element(by.css('.table'));
+    expect(table.isPresent()).toBe(false);
+    var chart = element(by.css('.nvd3-svg'));
+    expect(chart.isPresent()).toBe(true);
+
+    list.get(0).click();
+    var table = element(by.css('.table'));
+    expect(table.isPresent()).toBe(true);
+    var chart = element(by.css('.nvd3-svg'));
+    expect(chart.isPresent()).toBe(false);
   })
-
 
 });
 /* jshint ignore:end */
